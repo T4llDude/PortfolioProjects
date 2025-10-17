f8597f1a09d1e6cbd072bf39ad2ddd0d35eaa317
Christopher Cieri <cncieri@gmail.com>
2025-10-16 20:28:55 -0400

    Modified python file to match new dataset.

--- Python_projects/Enrollment_Data/examine_data.py
+++ Python_projects/Enrollment_Data/examine_data.py
@@ -1,13 +1,5 @@
 import pandas as pd 
 
 df = pd.read_csv('anonymized_enrollment_data.csv.csv', encoding='latin1')
-#print(cars_df.head(10))
 
-# All objects - no nulls
-print(df.info())
-
-# Identifying  numerical columns
-print(df.describe())
-
-# Count nbr of missing values (Missing values in CC/Battery Capacity - 3, Performance - 6, Torque - 1) 
-print(df.isnull().sum())
+print(df.head())
